61d9bd283349317c80d66dd32e052b845fc72bd1
Vignesh8995 <vigneshmanirathnam@gmail.com>
2021-03-21 14:26:09 +0530

    Donesuccusesly

--- src/org/gitptactice/GitDemoPract.java
+++ src/org/gitptactice/GitDemoPract.java
@@ -4,7 +4,8 @@ public class GitDemoPract {
 
 	public static void main(String[] args) {
 		System.out.println("stateent1");
-
+		System.out.println("stateent3");
+		System.out.println("stateenty 4");
 	}
 
 }
